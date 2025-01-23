00ea14f479bc1853421b4b3ae637f5c7d9d45b71
poomcha <ben.degeneve@gmail.com>
2025-01-23 14:12:37 +0100

    Create client

---
+++ client/src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import Layout from "./Layout.vue"
+</script>
+
+<template>
+  <Layout>
+    <!-- !!!
+     Create a QuestionAnswer component: display pairs of Question / Answer
+     Create a Answer component: display AI answers
+     Create a Question component: display User questions 
+     -->
+  </Layout>
+</template>
+
+<style lang="css"></style>
---
+++ client/src/Layout.vue
@@ -0,0 +1,23 @@
+<!-- Global Layout of the app -->
+
+<template>
+    <div class="layout">
+        <header></header>
+        <main>
+            <slot></slot>
+        </main>
+        <footer>
+            <!-- !!!
+            Integrate a list a link (email, linkedin, github) and a short paragraphe to contact me.
+              -->
+        </footer>
+    </div>
+</template>
+
+<style lang="css">
+.layout {
+    padding: 15px;
+    width: 100%;
+    min-height: 100vh;
+}
+</style>
